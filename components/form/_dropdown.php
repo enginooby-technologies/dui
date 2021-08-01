e450c6031d8362932688b40137c0090930ec9f3a
unknown <enginoobz@gmail.com>
2021-08-01 21:21:46 +0200

    [NEU] Init Dropdown component

--- components/form/_dropdown.php
+++ components/form/_dropdown.php
@@ -1,6 +1,6 @@
 <div class="dropdown" id="<?= $id ?>">
         <button type="button" class="dropdown-label"><?= $label ?></button>
-        <button type="button" class=" btn-primary dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <button type="button" class=" dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="fas fa-angle-down dropdown-arrow"></span>
                 <span class="sr-only">Toggle Dropdown</span>
         </button>
