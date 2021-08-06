b7c4c7ae2569dd0a9c50a39556ba22d03d70bd8d
unknown <enginoobz@gmail.com>
2021-08-06 07:44:13 +0200

    [Refactor] Separate references

--- js/main.js
+++ js/main.js
@@ -1,79 +1,4 @@
-/* FRAMEWORK  */
-const localFrameworkPath = '/dynamic-ui-framework';
-// remote paths: ad-hoc fallback to load file on remote server for different projects
-// TODO: resolve CORS or use CDN
-const remoteFrameworkPath = 'https://enginoobz.com/dynamic-ui-framework';
-export const settingFilePath = `${localFrameworkPath}/setting.html`;
-export const fallbackSettingFilePath = `${remoteFrameworkPath}/setting.html`;
-export const settingButtonFilePath = `${localFrameworkPath}/setting-button.html`;
-export const fallbackSettingButtonFilePath = `${remoteFrameworkPath}/setting-button.html`;
-export const dynamicUIJs = {
-    name: "DynamicUI.js",
-    src: `${localFrameworkPath}/js/DynamicUI.js`,
-    isModule: true,
-};
-export const fallbackDynamicUIJs = {
-    name: "DynamicUI.js",
-    src: `${remoteFrameworkPath}/js/DynamicUI.js`,
-    isModule: true,
-};
-/* DEPENDENCIES */
-// required CSS
-export const animateCss = {
-    name: "animate.min.css",
-    href: "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css",
-    // triggerClasses: ["animate__animated"]
-};
-export const bootstrapMinCss = {
-    name: "bootstrap.min.css",
-    href: "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css",
-    integrity: "sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-};
-export const fontawesomeCss = {
-    name: "fontawesome",
-    href: "https://use.fontawesome.com/releases/v5.15.0/css/all.css",
-    // triggerClasses: ["fa", "fab", "fas"]
-};
-// required JS
-const jqueryMinJs = {
-    name: "jquery",
-    src: "https://code.jquery.com/jquery-3.6.0.min.js",
-    integrity: "sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
-};
-const popperMinJs = {
-    name: "popper.min.js",
-    src: "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js",
-    integrity: "sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-};
-const bootstrapMinJs = {
-    name: "bootstrap.min.js",
-    src: "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js",
-    integrity: "sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-};
-const tinyColorMinJs = {
-    name: "tinycolor.min.js",
-    src: "https://cdnjs.cloudflare.com/ajax/libs/tinycolor/1.4.2/tinycolor.min.js",
-    integrity: "sha512-+aXA9mgbUvFe0ToTlbt8/3vT7+nOgUmFw29wfFCsGoh8AZMRSU0p4WtOvC1vkF2JBrndPN2TuNZsHPAKPPxe8Q=="
-};
-//optional
-const prismCoreMinJs = {
-    name: "prism-core.min.js",
-    src: "https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.1/components/prism-core.min.js",
-    integrity: "sha512-hM0R3pW9UdoNG9T+oIW5pG9ndvy3OKChFfVTKzjyxNW9xrt6vAbD3OeFWdSLQ8mjKSgd9dSO3RXn3tojQtiA8Q==",
-    triggerClasses: ["code-block"]
-};
-const prismAutoloaderMinJs = {
-    name: "prism-autoloader.min.js",
-    src: "https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.1/plugins/autoloader/prism-autoloader.min.js",
-    integrity: "sha512-xCfKr8zIONbip3Q1XG/u5x40hoJ0/DtP1bxyMEi0GWzUFoUffE+Dfw1Br8j55RRt9qG7bGKsh+4tSb1CvFHPSA==",
-    triggerClasses: ["code-block"]
-};
-const prismMinCss = {
-    name: "prism.min.css",
-    href: "https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.1/themes/prism.min.css",
-    integrity: "sha512-tN7Ec6zAFaVSG3TpNAKtk4DOHNpSwKHxxrsiw4GHKESGPs5njn/0sMCUMl2svV4wo4BK/rCP7juYz+zx+l6oeQ==",
-    triggerClasses: ["code-block"]
-};
+import * as Ref from "./references.js";
 /* CUSTOM PROJECT-SPECIFIC JS*/
 // dependent
 // let dependentJquery
@@ -83,21 +8,21 @@ let settingPanelLoaded = false;
 let settingButtonLoaded = false;
 let $settingButton;
 let $settingPanel;
-tryLoadStyleSheet(animateCss);
-tryLoadStyleSheet(fontawesomeCss);
-tryLoadStyleSheet(bootstrapMinCss);
+tryLoadStyleSheet(Ref.animateCss);
+tryLoadStyleSheet(Ref.fontawesomeCss);
+tryLoadStyleSheet(Ref.bootstrapMinCss);
 // in case jquery is included manually
 // TODO: load other jquery-dependent scripts also
-if (isScriptIncludedOrIgnored(jqueryMinJs))
+if (isScriptIncludedOrIgnored(Ref.jqueryMinJs))
     loadSettingButton();
 // defer loading dependencies (required)
-jqueryMinJs.onload = () => { tryLoadScript(popperMinJs); loadSettingButton(); }; // HTML/PHP loading dependent on jQuery
-popperMinJs.onload = () => tryLoadScript(bootstrapMinJs);
-tryLoadScript(jqueryMinJs);
+Ref.jqueryMinJs.onload = () => { tryLoadScript(Ref.popperMinJs); loadSettingButton(); }; // HTML/PHP loading dependent on jQuery
+Ref.popperMinJs.onload = () => tryLoadScript(Ref.bootstrapMinJs);
+tryLoadScript(Ref.jqueryMinJs);
 // defer loading dependencies (optional)
-tryLoadStyleSheet(prismMinCss);
-tryLoadScript(prismCoreMinJs);
-tryLoadScript(prismAutoloaderMinJs);
+tryLoadStyleSheet(Ref.prismMinCss);
+tryLoadScript(Ref.prismCoreMinJs);
+tryLoadScript(Ref.prismAutoloaderMinJs);
 // defer loading setting button
 function loadSettingButton() {
     // prevent loading multiple times
@@ -105,9 +30,9 @@ function loadSettingButton() {
         return;
     settingButtonLoaded = true;
     console.log(">>> Loading Setting Button");
-    loadFile(settingButtonFilePath)
+    loadFile(Ref.settingButtonFilePath)
         .done(setupButtonEvent)
-        .fail(() => loadFile(fallbackSettingButtonFilePath)
+        .fail(() => loadFile(Ref.fallbackSettingButtonFilePath)
         .done(setupButtonEvent));
 }
 function loadFile(filePath) {
@@ -147,9 +72,9 @@ function setupButtonEvent() {
     });
 }
 function loadSettingPanel() {
-    loadFile(settingFilePath)
+    loadFile(Ref.settingFilePath)
         .done(onSettingPanelLoaded)
-        .fail(() => loadFile(fallbackSettingFilePath)
+        .fail(() => loadFile(Ref.fallbackSettingFilePath)
         .done(onSettingPanelLoaded));
 }
 function onSettingPanelLoaded() {
@@ -157,10 +82,10 @@ function onSettingPanelLoaded() {
     $settingPanel = $("#setting-section .setting-panel");
     toggleSettingPanel();
     // lazy loading main framework script
-    dynamicUIJs.onerror = () => tryLoadScript(fallbackDynamicUIJs);
-    tryLoadScript(dynamicUIJs);
+    Ref.dynamicUIJs.onerror = () => tryLoadScript(Ref.fallbackDynamicUIJs);
+    tryLoadScript(Ref.dynamicUIJs);
     // lazy loading dependencies
-    tryLoadScript(tinyColorMinJs);
+    tryLoadScript(Ref.tinyColorMinJs);
 }
 function toggleSettingPanel() {
     $settingPanel.toggleClass('show');
---
+++ js/references.js
@@ -0,0 +1,82 @@
+/*-------------------------
+      Framework Resources
+-------------------------*/
+const localFrameworkPath = '/dynamic-ui-framework';
+// remote paths: ad-hoc fallback to load file on remote server for different projects
+// TODO: resolve CORS or use CDN
+const remoteFrameworkPath = 'https://enginoobz.com/dynamic-ui-framework';
+export const settingFilePath = `${localFrameworkPath}/setting.html`;
+export const fallbackSettingFilePath = `${remoteFrameworkPath}/setting.html`;
+export const settingButtonFilePath = `${localFrameworkPath}/setting-button.html`;
+export const fallbackSettingButtonFilePath = `${remoteFrameworkPath}/setting-button.html`;
+export const dynamicUIJs = {
+    name: "DynamicUI.js",
+    src: `${localFrameworkPath}/js/DynamicUI.js`,
+    isModule: true,
+};
+export const fallbackDynamicUIJs = {
+    name: "DynamicUI.js",
+    src: `${remoteFrameworkPath}/js/DynamicUI.js`,
+    isModule: true,
+};
+/*-------------------------
+      Required Resources
+-------------------------*/
+/*Required CSS */
+export const animateCss = {
+    name: "animate.min.css",
+    href: "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css",
+    // triggerClasses: ["animate__animated"]
+};
+export const bootstrapMinCss = {
+    name: "bootstrap.min.css",
+    href: "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css",
+    integrity: "sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+};
+export const fontawesomeCss = {
+    name: "fontawesome",
+    href: "https://use.fontawesome.com/releases/v5.15.0/css/all.css",
+    // triggerClasses: ["fa", "fab", "fas"]
+};
+/*Required JS */
+export const jqueryMinJs = {
+    name: "jquery",
+    src: "https://code.jquery.com/jquery-3.6.0.min.js",
+    integrity: "sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+};
+export const popperMinJs = {
+    name: "popper.min.js",
+    src: "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js",
+    integrity: "sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+};
+export const bootstrapMinJs = {
+    name: "bootstrap.min.js",
+    src: "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js",
+    integrity: "sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+};
+export const tinyColorMinJs = {
+    name: "tinycolor.min.js",
+    src: "https://cdnjs.cloudflare.com/ajax/libs/tinycolor/1.4.2/tinycolor.min.js",
+    integrity: "sha512-+aXA9mgbUvFe0ToTlbt8/3vT7+nOgUmFw29wfFCsGoh8AZMRSU0p4WtOvC1vkF2JBrndPN2TuNZsHPAKPPxe8Q=="
+};
+/*-------------------------
+      Optional Resources (defer loading when needed)
+-------------------------*/
+export const prismCoreMinJs = {
+    name: "prism-core.min.js",
+    src: "https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.1/components/prism-core.min.js",
+    integrity: "sha512-hM0R3pW9UdoNG9T+oIW5pG9ndvy3OKChFfVTKzjyxNW9xrt6vAbD3OeFWdSLQ8mjKSgd9dSO3RXn3tojQtiA8Q==",
+    triggerClasses: ["code-block"]
+};
+export const prismAutoloaderMinJs = {
+    name: "prism-autoloader.min.js",
+    src: "https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.1/plugins/autoloader/prism-autoloader.min.js",
+    integrity: "sha512-xCfKr8zIONbip3Q1XG/u5x40hoJ0/DtP1bxyMEi0GWzUFoUffE+Dfw1Br8j55RRt9qG7bGKsh+4tSb1CvFHPSA==",
+    triggerClasses: ["code-block"]
+};
+export const prismMinCss = {
+    name: "prism.min.css",
+    href: "https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.1/themes/prism.min.css",
+    integrity: "sha512-tN7Ec6zAFaVSG3TpNAKtk4DOHNpSwKHxxrsiw4GHKESGPs5njn/0sMCUMl2svV4wo4BK/rCP7juYz+zx+l6oeQ==",
+    triggerClasses: ["code-block"]
+};
