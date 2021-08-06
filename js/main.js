c3eae360b192f07fe6c6b67c0c50a0a08aa86a56
unknown <enginoobz@gmail.com>
2021-08-06 06:07:52 +0200

    [Refactor] Dynamic loading CSS with interface

--- js/main.js
+++ js/main.js
@@ -2,20 +2,32 @@
 /* FRAMEWORK RESOURCES FOR LAZY LOADING */
 const localFrameworkPath = '/dynamic-ui-framework';
 const remoteFrameworkPath = 'https://enginoobz.com/dynamic-ui-framework';
+// local paths
 const settingFilePath = `${localFrameworkPath}/setting.html`;
 const settingButtonFilePath = `${localFrameworkPath}/setting-button.html`;
 const dynamicUIFilePath = `${localFrameworkPath}/js/DynamicUI.js`;
-// ad-hoc fallback to load file on remote server for different projects
+// remote paths: ad-hoc fallback to load file on remote server for different projects
 // TODO: resolve CORS or use CDN
 const fallbackSettingFilePath = `${remoteFrameworkPath}/setting.html`;
 const fallbackSettingButtonFilePath = `${remoteFrameworkPath}/setting-button.html`;
 const fallbackDynamicUIFilePath = `${remoteFrameworkPath}/js/DynamicUI.js`;
 /* DEPENDENCIES */
 // required CSS
-const animateCss = "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css";
-const fontawesomeCss = "https://use.fontawesome.com/releases/v5.15.0/css/all.css";
-const bootstrapMinCss = "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css";
-const bootstrapMinCssIntegrity = "sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T";
+const animateCss = {
+    name: "animate.min.css",
+    href: "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css",
+    // triggerClasses: ["animate__animated"]
+};
+const bootstrapMinCss = {
+    name: "bootstrap.min.css",
+    href: "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css",
+    integrity: "sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+};
+const fontawesomeCss = {
+    name: "fontawesome",
+    href: "https://use.fontawesome.com/releases/v5.15.0/css/all.css",
+    triggerClasses: ["fa", "fab", "fas"]
+};
 // required JS
 const jqueryMinJs = "https://code.jquery.com/jquery-3.6.0.min.js";
 const jqueryMinJsIntegrity = "sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=";
@@ -30,25 +42,25 @@ const prismCoreMinJs = "https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.1/comp
 const prismCoreMinJsIntegrity = "sha512-hM0R3pW9UdoNG9T+oIW5pG9ndvy3OKChFfVTKzjyxNW9xrt6vAbD3OeFWdSLQ8mjKSgd9dSO3RXn3tojQtiA8Q==";
 const prismAutoloaderMinJs = "https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.1/plugins/autoloader/prism-autoloader.min.js";
 const prismAutoloaderMinJsIntegrity = "sha512-xCfKr8zIONbip3Q1XG/u5x40hoJ0/DtP1bxyMEi0GWzUFoUffE+Dfw1Br8j55RRt9qG7bGKsh+4tSb1CvFHPSA==";
-const prismMinCss = "https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.1/themes/prism.min.css";
-const prismMinCssIntegrity = "sha512-tN7Ec6zAFaVSG3TpNAKtk4DOHNpSwKHxxrsiw4GHKESGPs5njn/0sMCUMl2svV4wo4BK/rCP7juYz+zx+l6oeQ==";
+const prismMinCss = {
+    name: "prism.min.css",
+    href: "https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.1/themes/prism.min.css",
+    integrity: "sha512-tN7Ec6zAFaVSG3TpNAKtk4DOHNpSwKHxxrsiw4GHKESGPs5njn/0sMCUMl2svV4wo4BK/rCP7juYz+zx+l6oeQ==",
+    triggerClasses: ["code-block"]
+};
+/* CUSTOM PROJECT-SPECIFIC JS*/
+// dependent
+// let dependentJquery
 const scripts = document.getElementsByTagName("script");
 const links = document.getElementsByTagName("link");
 let settingPanelLoaded = false;
 let $settingButton;
 let $settingPanel;
-// only load CSS if its class is used in page
-// CAUTION: handle case where element with this class is loaded later into the page
-// if (document.querySelector('.animate__animated'))
-if (!isStyleSheetAlreadyIncluded('animate.css'))
-    loadStyleSheet(animateCss);
-// if (document.querySelector('.fa') || document.querySelector('.fas'))
-if (!isStyleSheetAlreadyIncluded('fontawesome.css'))
-    loadStyleSheet(fontawesomeCss);
-if (!isStyleSheetAlreadyIncluded('bootstrap.min.css'))
-    loadStyleSheet(bootstrapMinCss, bootstrapMinCssIntegrity);
+tryLoadStyleSheet(animateCss);
+tryLoadStyleSheet(fontawesomeCss);
+tryLoadStyleSheet(bootstrapMinCss);
 // defer loading dependencies (required)
-if (!isScriptAlreadyIncluded('jquery')) {
+if (!isScriptIncludedOrIgnored('jquery')) {
     loadScript(jqueryMinJs, jqueryMinJsIntegrity, undefined, () => {
         loadScript(popperMinJs, popperMinJsIntegrity, undefined, () => {
             loadScript(bootstrapMinJs, bootstrapMinJsIntegrity, undefined, loadSettingButton);
@@ -58,14 +70,12 @@ if (!isScriptAlreadyIncluded('jquery')) {
 else
     loadSettingButton();
 // defer loading dependencies (optional)
-// only load Prism if Code block component is used in page
+tryLoadStyleSheet(prismMinCss);
 if (document.querySelector('.code-block')) {
-    if (!isStyleSheetAlreadyIncluded('prism.min.css'))
-        loadStyleSheet(prismMinCss, prismMinCssIntegrity);
     // TODO: manually activate Prism to highlight code after load
-    if (!isScriptAlreadyIncluded('prism-core.min.js'))
+    if (!isScriptIncludedOrIgnored('prism-core.min.js'))
         loadScript(prismCoreMinJs, prismCoreMinJsIntegrity);
-    if (!isScriptAlreadyIncluded('prism-autoloader.min.js'))
+    if (!isScriptIncludedOrIgnored('prism-autoloader.min.js'))
         loadScript(prismAutoloaderMinJs, prismAutoloaderMinJsIntegrity);
 }
 // defer loading setting button
@@ -80,19 +90,23 @@ function loadFile(filePath) {
         $('body').append(data);
     });
 }
-// to prevent framework from loading a script/sheet, add a fake tag including the name file of the resource 
-// e.g., <script async src="dont-load-prism-core.min.js-please "></script>
-function isScriptAlreadyIncluded(scriptName) {
-    var _a;
+// to prevent framework from loading a script/style shet
+// e.g., <script data-ignore="dont-load-prism-core.min.js-please "></script>
+function isScriptIncludedOrIgnored(scriptName) {
+    var _a, _b;
     for (var i = 0; i < scripts.length; i++)
-        if ((_a = scripts[i].getAttribute('src')) === null || _a === void 0 ? void 0 : _a.includes(scriptName))
+        if (((_a = scripts[i].getAttribute('src')) === null || _a === void 0 ? void 0 : _a.includes(scriptName))
+            ||
+                ((_b = scripts[i].getAttribute('data-ignore')) === null || _b === void 0 ? void 0 : _b.includes(scriptName)))
             return true;
     return false;
 }
-function isStyleSheetAlreadyIncluded(sheetName) {
-    var _a;
+function isStyleSheetIncludedOrIgnored(sheet) {
+    var _a, _b;
     for (var i = 0; i < links.length; i++)
-        if ((_a = links[i].getAttribute('href')) === null || _a === void 0 ? void 0 : _a.includes(sheetName))
+        if (((_a = links[i].getAttribute('href')) === null || _a === void 0 ? void 0 : _a.includes(sheet.name))
+            ||
+                ((_b = links[i].getAttribute('data-ignore')) === null || _b === void 0 ? void 0 : _b.includes(sheet.name)))
             return true;
     return false;
 }
@@ -142,13 +156,30 @@ function loadScript(src, integrity, isModule, onload, onerror) {
     }
     document.head.appendChild(script);
 }
-function loadStyleSheet(href, integrity) {
+function tryLoadStyleSheet(sheet) {
+    var _a;
+    let isFileNeeded = true;
+    if (sheet.triggerClasses) {
+        isFileNeeded = false;
+        for (let className of sheet.triggerClasses) {
+            if (document.querySelector("." + className))
+                isFileNeeded = true;
+            break;
+        }
+    }
+    (_a = sheet.triggerClasses) === null || _a === void 0 ? void 0 : _a.forEach(className => { if (document.querySelector("." + className))
+        isFileNeeded = true; });
+    if (isFileNeeded && !isStyleSheetIncludedOrIgnored(sheet))
+        loadStyleSheet(sheet);
+}
+function loadStyleSheet(sheet) {
+    console.log(`>>> Loading ${sheet.name.toLocaleUpperCase()} from ${sheet.href}`);
     const link = document.createElement('link');
-    link.href = href;
+    link.href = sheet.href;
     link.rel = 'stylesheet';
     link.type = 'text/css';
-    if (integrity) {
-        link.integrity = integrity;
+    if (sheet.integrity) {
+        link.integrity = sheet.integrity;
         link.crossOrigin = "anonymous";
         link.referrerPolicy = "no-referrer";
     }
--- ts/main.ts
+++ ts/main.ts
@@ -1,23 +1,52 @@
 /* FRAMEWORK RESOURCES FOR LAZY LOADING */
 const localFrameworkPath: string = '/dynamic-ui-framework';
 const remoteFrameworkPath: string = 'https://enginoobz.com/dynamic-ui-framework';
-
+// local paths
 const settingFilePath: string = `${localFrameworkPath}/setting.html`;
 const settingButtonFilePath: string = `${localFrameworkPath}/setting-button.html`;
 const dynamicUIFilePath: string = `${localFrameworkPath}/js/DynamicUI.js`;
-
-// ad-hoc fallback to load file on remote server for different projects
+// remote paths: ad-hoc fallback to load file on remote server for different projects
 // TODO: resolve CORS or use CDN
 const fallbackSettingFilePath: string = `${remoteFrameworkPath}/setting.html`;
 const fallbackSettingButtonFilePath: string = `${remoteFrameworkPath}/setting-button.html`;
 const fallbackDynamicUIFilePath: string = `${remoteFrameworkPath}/js/DynamicUI.js`;
 
+// @name is substring of src/href to check if the file is already included manually with <script/link>
+//@triggerClasses are classes needed to appear in page for the file to be loaded 
+//TODO: handle case where element with this class is loaded later into the page
+interface ScripFile {
+        name: string;
+        src: string,
+        isModule?: boolean;
+        integrity?: string;
+        onload?: () => void;
+        onerror?: () => void;
+        triggerClasses?: string[],
+}
+interface StyleSheetFile {
+        name: string;
+        href: string,
+        integrity?: string,
+        triggerClasses?: string[],
+}
+
 /* DEPENDENCIES */
 // required CSS
-const animateCss = "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css";
-const fontawesomeCss = "https://use.fontawesome.com/releases/v5.15.0/css/all.css";
-const bootstrapMinCss = "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css";
-const bootstrapMinCssIntegrity = "sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T";
+const animateCss: StyleSheetFile = {
+        name: "animate.min.css",
+        href: "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css",
+        // triggerClasses: ["animate__animated"]
+};
+const bootstrapMinCss: StyleSheetFile = {
+        name: "bootstrap.min.css",
+        href: "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css",
+        integrity: "sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+}
+const fontawesomeCss: StyleSheetFile = {
+        name: "fontawesome",
+        href: "https://use.fontawesome.com/releases/v5.15.0/css/all.css",
+        triggerClasses: ["fa", "fab", "fas"]
+}
 // required JS
 const jqueryMinJs = "https://code.jquery.com/jquery-3.6.0.min.js";
 const jqueryMinJsIntegrity = "sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=";
@@ -32,8 +61,16 @@ const prismCoreMinJs = "https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.1/comp
 const prismCoreMinJsIntegrity = "sha512-hM0R3pW9UdoNG9T+oIW5pG9ndvy3OKChFfVTKzjyxNW9xrt6vAbD3OeFWdSLQ8mjKSgd9dSO3RXn3tojQtiA8Q=="
 const prismAutoloaderMinJs = "https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.1/plugins/autoloader/prism-autoloader.min.js";
 const prismAutoloaderMinJsIntegrity = "sha512-xCfKr8zIONbip3Q1XG/u5x40hoJ0/DtP1bxyMEi0GWzUFoUffE+Dfw1Br8j55RRt9qG7bGKsh+4tSb1CvFHPSA==";
-const prismMinCss = "https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.1/themes/prism.min.css";
-const prismMinCssIntegrity = "sha512-tN7Ec6zAFaVSG3TpNAKtk4DOHNpSwKHxxrsiw4GHKESGPs5njn/0sMCUMl2svV4wo4BK/rCP7juYz+zx+l6oeQ=="
+const prismMinCss: StyleSheetFile = {
+        name: "prism.min.css",
+        href: "https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.1/themes/prism.min.css",
+        integrity: "sha512-tN7Ec6zAFaVSG3TpNAKtk4DOHNpSwKHxxrsiw4GHKESGPs5njn/0sMCUMl2svV4wo4BK/rCP7juYz+zx+l6oeQ==",
+        triggerClasses: ["code-block"]
+}
+
+/* CUSTOM PROJECT-SPECIFIC JS*/
+// dependent
+// let dependentJquery
 
 const scripts = document.getElementsByTagName("script");
 const links = document.getElementsByTagName("link");
@@ -41,16 +78,12 @@ let settingPanelLoaded: boolean = false;
 let $settingButton: JQuery<HTMLElement>;
 let $settingPanel: JQuery<HTMLElement>;
 
-// only load CSS if its class is used in page
-// CAUTION: handle case where element with this class is loaded later into the page
-// if (document.querySelector('.animate__animated'))
-if (!isStyleSheetAlreadyIncluded('animate.css')) loadStyleSheet(animateCss);
-// if (document.querySelector('.fa') || document.querySelector('.fas'))
-if (!isStyleSheetAlreadyIncluded('fontawesome.css')) loadStyleSheet(fontawesomeCss);
-if (!isStyleSheetAlreadyIncluded('bootstrap.min.css')) loadStyleSheet(bootstrapMinCss, bootstrapMinCssIntegrity);
+tryLoadStyleSheet(animateCss);
+tryLoadStyleSheet(fontawesomeCss);
+tryLoadStyleSheet(bootstrapMinCss);
 
 // defer loading dependencies (required)
-if (!isScriptAlreadyIncluded('jquery')) {
+if (!isScriptIncludedOrIgnored('jquery')) {
         loadScript(jqueryMinJs, jqueryMinJsIntegrity, undefined, () => {
                 loadScript(popperMinJs, popperMinJsIntegrity, undefined, () => {
                         loadScript(bootstrapMinJs, bootstrapMinJsIntegrity, undefined, loadSettingButton)
@@ -59,12 +92,13 @@ if (!isScriptAlreadyIncluded('jquery')) {
 } else loadSettingButton();
 
 // defer loading dependencies (optional)
-// only load Prism if Code block component is used in page
+tryLoadStyleSheet(prismMinCss);
 if (document.querySelector('.code-block')) {
-        if (!isStyleSheetAlreadyIncluded('prism.min.css')) loadStyleSheet(prismMinCss, prismMinCssIntegrity);
         // TODO: manually activate Prism to highlight code after load
-        if (!isScriptAlreadyIncluded('prism-core.min.js')) loadScript(prismCoreMinJs, prismCoreMinJsIntegrity);
-        if (!isScriptAlreadyIncluded('prism-autoloader.min.js')) loadScript(prismAutoloaderMinJs, prismAutoloaderMinJsIntegrity);
+        if (!isScriptIncludedOrIgnored('prism-core.min.js'))
+                loadScript(prismCoreMinJs, prismCoreMinJsIntegrity);
+        if (!isScriptIncludedOrIgnored('prism-autoloader.min.js'))
+                loadScript(prismAutoloaderMinJs, prismAutoloaderMinJsIntegrity);
 }
 
 // defer loading setting button
@@ -81,17 +115,23 @@ function loadFile(filePath: string) {
         })
 }
 
-// to prevent framework from loading a script/sheet, add a fake tag including the name file of the resource 
-// e.g., <script async src="dont-load-prism-core.min.js-please "></script>
-function isScriptAlreadyIncluded(scriptName: string) {
+// to prevent framework from loading a script/style shet
+// e.g., <script data-ignore="dont-load-prism-core.min.js-please "></script>
+function isScriptIncludedOrIgnored(scriptName: string) {
         for (var i = 0; i < scripts.length; i++)
-                if (scripts[i].getAttribute('src')?.includes(scriptName)) return true;
+                if (scripts[i].getAttribute('src')?.includes(scriptName)
+                        ||
+                        scripts[i].getAttribute('data-ignore')?.includes(scriptName))
+                        return true;
         return false;
 }
 
-function isStyleSheetAlreadyIncluded(sheetName: string) {
+function isStyleSheetIncludedOrIgnored(sheet: StyleSheetFile) {
         for (var i = 0; i < links.length; i++)
-                if (links[i].getAttribute('href')?.includes(sheetName)) return true;
+                if (links[i].getAttribute('href')?.includes(sheet.name)
+                        ||
+                        links[i].getAttribute('data-ignore')?.includes(sheet.name))
+                        return true;
         return false;
 }
 
@@ -141,13 +181,27 @@ function loadScript(src: string, integrity?: string, isModule?: boolean, onload?
         document.head.appendChild(script);
 }
 
-function loadStyleSheet(href: string, integrity?: string) {
+function tryLoadStyleSheet(sheet: StyleSheetFile) {
+        let isFileNeeded: boolean = true;
+        if (sheet.triggerClasses) {
+                isFileNeeded = false;
+                for (let className of sheet.triggerClasses) {
+                        if (document.querySelector("." + className)) isFileNeeded = true;
+                        break;
+                }
+        }
+        sheet.triggerClasses?.forEach(className => { if (document.querySelector("." + className)) isFileNeeded = true; })
+        if (isFileNeeded && !isStyleSheetIncludedOrIgnored(sheet)) loadStyleSheet(sheet);
+}
+
+function loadStyleSheet(sheet: StyleSheetFile) {
+        console.log(`>>> Loading ${sheet.name.toLocaleUpperCase()} from ${sheet.href}`);
         const link: HTMLLinkElement = document.createElement('link');
-        link.href = href;
+        link.href = sheet.href;
         link.rel = 'stylesheet';
         link.type = 'text/css';
-        if (integrity) {
-                link.integrity = integrity;
+        if (sheet.integrity) {
+                link.integrity = sheet.integrity;
                 link.crossOrigin = "anonymous";
                 link.referrerPolicy = "no-referrer";
         }
