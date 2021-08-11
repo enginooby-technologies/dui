f09c39fa24e7b2e294f1d7bc234695a8e0252770
unknown <enginoobz@gmail.com>
2021-08-11 10:58:54 +0200

    Create template class for components

---
+++ components/Template.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+include_once __DIR__ . "/../Component.php";
+
+function T(?string $id = null, string $wrapperClass = '', string $class = '', ?string $onclick = null)
+{
+        return new T($id, $class, $wrapperClass, $onclick);
+}
+
+class T extends Component
+{
+        // traits
+        public function __construct(
+                //protected members from traits
+
+                ?string $id = null,
+                string $class = '',
+                string $wrapperClass = '',
+                ?string $onclick = null,
+                string $view = '/T/T_view.php',
+        ) {
+                parent::__construct($id, $class, $wrapperClass, $onclick, $view);
+        }
+}
