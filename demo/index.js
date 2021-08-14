25ccebfac8be1cd6807d8314865c757b55d6e80b
unknown <enginoobz@gmail.com>
2021-08-14 22:10:40 +0200

    [Setup] Link with DUI DOM library using Project References

--- demo/index.js
+++ demo/index.js
@@ -1,3 +1,3 @@
-import { Button } from '../js/components/basic/Button.js';
+import { Button } from '../../dui-dom/js/basic/Button.js';
 var btn1 = Button("btn1");
 btn1.onClick = function (event) { return alert("Hi! I am " + btn1.label); };
--- demo/index.ts
+++ demo/index.ts
@@ -1,4 +1,4 @@
-import { Button } from '../js/components/basic/Button.js'
+import { Button } from '../../dui-dom/js/basic/Button.js'
 
 const btn1 = Button("btn1")
 btn1.onClick = event => alert(`Hi! I am ${btn1.label}`)
